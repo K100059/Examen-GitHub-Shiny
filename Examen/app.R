c7fd67f0a99ff4cf9510fea73df707b972675e51
K100059 <camille.meurisse59@gmail.com>
2025-12-07 16:15:26 +0000

    Ajouts et connexions de slicers au graph

--- Examen/app.R
+++ Examen/app.R
@@ -4,23 +4,41 @@ library(ggplot2)
 library(dplyr)
 library(DT)
 library(bslib)
+library(plotly)
 
 # UI ----
 ui <- fluidPage(
+  
+  theme = bs_theme(
+    version = 5,
+    bootswatch = "lumen"),
   
   titlePanel("Exploration des Diamants"),
   
   sidebarLayout(
     sidebarPanel(
-      sliderInput("prixmax",
+      sliderInput("price",
                   "Prix maximum :",
                   min = 300,
                   max = 20000,
-                  value = 500)
+                  value = 5000),
+      
+      
+      selectInput(inputId = "color",
+                  label = "Choisir une couleur à filtrer :",
+                  choices = c("D","E", "F", "G", "H", "I", "J" ),
+                  selected = "D"
+      ),
+      
+      actionButton(inputId = "bouton",
+                   label = "Visualiser le graph"
+      )
     ),
     
     mainPanel(
-      plotOutput("distPlot")
+      plotlyOutput("DiamondsPlot"),
+      textOutput("nombreLignes"),
+      DT::DTOutput("tableau")
     )
   )
 )
@@ -28,11 +46,27 @@ ui <- fluidPage(
 # SERVEUR ----
 server <- function(input, output) {
   
+  rv <- reactiveValues()
   
-  
-  output$distPlot <- renderPlot({
+  observeEvent(input$bouton, {
+    message(showNotification(
+      "La valeur du slider a changé !",
+      type = "message"
+    ))
     
-    ggplot(diamonds, aes(x=carat, y=price)) + geom_point()
+    rv$str <- diamonds |> 
+      filter(price >= input$price & color == input$color)
+    
+    rv$nuage <- rv$str |>
+      ggplot(aes(x=carat, y=price)) + 
+      geom_point(
+        alpha = 0.5) +
+      ggtitle(paste("prix:", input$price, " & color:", input$color))
+    
+    
+    output$DiamondsPlot <- plotly::renderPlotly({
+      rv$nuage
+    })
   })
 }
 
